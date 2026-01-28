2644b3b9a3c45b47639dc0c7c03388ac54f4d400
Faryzal2020 <fary290796@gmail.com>
2026-01-28 18:45:31 +0700

    update

--- src/hooks/useBusMonitoring.js
+++ src/hooks/useBusMonitoring.js
@@ -1,7 +1,7 @@
 import { useState, useEffect, useRef } from 'react';
 import { io } from 'socket.io-client';
 
-const CENTRAL_WS_URL = 'http://192.168.18.234:8000';
+const CENTRAL_WS_URL = 'http://192.168.18.234:8002';
 const DIRECT_CHECK_PORT = 3005;
 
 export function useBusMonitoring() {
