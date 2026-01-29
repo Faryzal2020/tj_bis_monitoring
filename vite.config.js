15b3c6704c1855c3a66a7080fd27dc521538baae
Faryzal2020 <fary290796@yahoo.com>
2026-01-29 13:47:43 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-        host: true
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '')
+    return {
+        plugins: [react()],
+        server: {
+            port: parseInt(env.PORT) || 4173,
+            host: true
+        }
     }
 })
